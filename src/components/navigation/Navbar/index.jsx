7030bf4353f8a1b742328b7d763bfa5c2a9a4785
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-01-16 10:36:03 -0600

    Add NavLinks to desktops and laptops devices

--- src/components/navigation/Navbar/index.jsx
+++ src/components/navigation/Navbar/index.jsx
@@ -1,7 +1,8 @@
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import Logo from '../../Logo/index.jsx';
 import Hamburger from '../Hamburger/index.jsx';
-import {NavbarWrapper} from '../../../styles/Navbar.styled.jsx';
+import {NavbarWrapper, NavLinkWrapper, StyledNavLink} from '../../../styles/Navbar.styled.jsx';
 
 
 /*const Nav = styled.nav`
@@ -18,7 +19,20 @@ import {NavbarWrapper} from '../../../styles/Navbar.styled.jsx';
 `;*/
 
 export default function Navbar() {
-
+   const [active, setActive] = useState(false);
+   const links = [
+      {
+         page: 'Home',
+         href: '/home'
+      },
+      {page: 'About', href: '/about'},
+      {page: 'Products', href: '/products'},
+      {
+         page: 'Contact',
+         href: '/contact'
+      },
+      {page: 'Get Started', href: '/sign-in'}
+   ];
    return (
       <section className='utils-container-fluid navbar-section'>
          <div className='utils-container'>
@@ -27,7 +41,19 @@ export default function Navbar() {
                   <Logo />
                   <span className='logo__brand--color'>e-Mart</span>
                </div>
-
+               <Hamburger />
+               <NavLinkWrapper active={active}>
+                  {links.map((link) => (
+                     <StyledNavLink
+                        activeclassname='active'
+                        key={link.page}
+                        to={link.href}
+                        onClick={() => setActive(false)}
+                     >
+                        {link.page}
+                     </StyledNavLink>
+                  ))}
+               </NavLinkWrapper>
             </NavbarWrapper>
          </div>
       </section>
--- src/styles/Navbar.styled.jsx
+++ src/styles/Navbar.styled.jsx
@@ -22,3 +22,45 @@ export const NavbarWrapper = styled.nav`
      font-size: 1.5rem;
   }
 `;
+
+export const NavLinkWrapper = styled.div`
+  @media (max-width: 704px) {
+    display: ${(props) => (!props.active ? 'none' : 'block')};
+    text-align: center;
+    padding: 2rem 0;
+  }
+`;
+
+export const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+   font-weight: 800;
+  transition: 0.2s;
+  color: hsl(210, 0%, 98%);
+  margin-left: 2rem;
+   
+  &:last-child {
+    /*background: #007dfc;*/
+    background: hsl(196.18, 77.39%, 54.9%);
+    padding: 0.5rem 1rem;
+    border-radius: 24px;
+    &:hover {
+      color: hsl(210, 0%, 98%)
+    }
+    &.${(props) => props.activeclassname} {
+      color: hsl(210, 0%, 98%);
+    }
+  }
+
+  &.${(props) => props.activeclassname} {
+    color: hsl(196, 77%, 55%);
+  }
+
+  &:hover {
+     color: hsl(196, 77%, 55%);
+  }
+
+  @media (max-width: 704px) {
+    display: block;
+    margin: 2rem auto;
+  }
+`;
